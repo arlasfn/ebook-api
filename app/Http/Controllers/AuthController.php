9078e2ffc88fe2ac1beb8c1c0aa1d735da86995a
arlasfn <putrisifhana17@gmail.com>
2020-08-14 20:22:15 +0700

    endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me() {
+        return "NIS : 3103118024
+        Name : Arla Sifhana Putri
+        Gender : Female
+        Phone : 081227883644
+        Class : XII RPL 1";
+    }
+}
